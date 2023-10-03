e8fdcf1b7a148beef3023b715788f22d247e9321
Tanu <tanuk421@gmail.com>
2023-10-03 10:32:51 +0900

    starting week1

---
+++ Week1_lectures.R
@@ -0,0 +1,6 @@
+#Steps in Data analysis: (Raw data -> Processing script -> Tidy data) -> data analysis -> data communication
+##Data cleaning needs 4 things:
+        #1. The raw data
+        #2. A tidy dataset
+        #3. metadata: A code book describing each variable and its values in tidy dataset
+        #4. Recipe used to go from above 1,2,3 points
